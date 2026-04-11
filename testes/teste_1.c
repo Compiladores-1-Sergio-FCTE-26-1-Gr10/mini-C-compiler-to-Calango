2bf04de3533a874a5826c0465c56147919e3fa0f
jadequilin <jplyra522@gmail.com>
2026-04-11 15:21:28 -0300

    Tasks do analisador lexico e semantico, já com a ação semantica

---
+++ testes/teste_1.c
@@ -0,0 +1,15 @@
+int main() {
+    int x = 10;
+    float x = 3.14;
+    if (x > 5) {
+        printf("maior");
+    } else {
+        x = x + 1;
+    }
+    while (x < 20) {
+        x = x + 1;
+    }
+}
+
+
+
